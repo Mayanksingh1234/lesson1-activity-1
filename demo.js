312cf39636351759ca76b75ade8c644b3be50546
Mayank Singh <singhmayank2702@gmail.com>
2026-03-25 21:46:31 +0530

    fadfdsfdfadfdfa

---
+++ demo.js
@@ -0,0 +1,6 @@
+function rotateArray(arr, k){
+    k = k % arr.length
+    return arr.slice(-k).concat(arr.slice(0, arr.length-k))
+}
+
+console.log(rotateArray([1,2,3,4,5],2))
---
+++ dmo.js
@@ -0,0 +1,9 @@
+let arr=['a','b','a','c','b']
+let map={}
+function count(arr){
+    for(num of arr){
+        map[num]+=(undefined?)
+    }
+    return map
+}
+console.log(count(arr))
